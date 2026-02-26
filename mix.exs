e7ef9dd0bb3decf44e835dce7dafe6158880a883
Maxime Buffa <mbuffa@users.noreply.github.com>
2026-02-26 16:53:15 +0100

    Solve deprecations and update dev/test dependencies

--- mix.exs
+++ mix.exs
@@ -69,7 +69,7 @@ defmodule ExAws.STS.Mixfile do
   defp ex_aws() do
     case System.get_env("AWS") do
       "LOCAL" -> {:ex_aws, path: "../ex_aws"}
-      _ -> {:ex_aws, "~> 2.2"}
+      _ -> {:ex_aws, "~> 2.3"}
     end
   end
 end
